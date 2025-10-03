fa837b48a06e1d557ce8b8e5b1f17bc8a43d94f2
Sai Vivek <vivekvallabhaneni0@gmail.com>
2025-10-03 10:35:50 +0530

    Commit

---
+++ src/main/java/org/Numbers.java
@@ -0,0 +1,16 @@
+package org;
+
+public class Numbers {
+    public static void main(String[] args){
+        Numbers a1=new Numbers();
+        a1.num();
+    }
+    public void num(){
+        for (int i=1;i <= 100;i++){
+//            a+=1;
+            System.out.println(i);
+
+        }
+    }
+}
+
--- src/main/java/org/Prime.java
+++ src/main/java/org/Prime.java
@@ -2,6 +2,11 @@ package org;
 
 public class Prime {
     public static  void main(String[] args){
+        Prime a=new Prime();
+        a.num(6);
+
+    }
+    public void num(int a){
 
     }
 }
